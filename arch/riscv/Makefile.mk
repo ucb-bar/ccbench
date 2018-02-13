6b52a294f4a1f3c17ad37c601f1129e6c71c7aba
Donggyu Kim <dgkim@eecs.berkeley.edu>
2018-02-13 09:38:40 -0800

    fix riscv compiler in makefile

--- arch/riscv/Makefile.mk
+++ arch/riscv/Makefile.mk
@@ -2,11 +2,12 @@
 # Architecture-specific Makefile fragment
 ##########################################
 
-CC=riscv-gcc
+CC=riscv64-unknown-elf-gcc
 
 # for now, compile common/barrier.* using our own barrier implementations.
 # if your compiler cant handle march, you're using an older version of gcc
-CFLAGS=-Wa,-march=RVIMAFDXhwacha -std=gnu99 -O2 -ffast-math -funroll-loops -I../common 
+CFLAGS=-Wa,-march=rv64imafd -std=gnu99 -O2 -ffast-math -funroll-loops -I../common 
+#CFLAGS=-Wa,-march=RVIMAFDXhwacha -std=gnu99 -O2 -ffast-math -funroll-loops -I../common 
 #CFLAGS=-std=c99 -mtune=native -march=native -mssse3 -O3 -funroll-loops -I../common -DWITH_BARRIER -Wall
 	
 LD_FLAGS= -ffast-math
