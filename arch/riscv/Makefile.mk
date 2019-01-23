e835d8786c2f2f1b9f588bea881936f5a917581f
Sagar Karandikar <sagark@eecs.berkeley.edu>
2019-01-23 21:37:43 +0000

    remove old arch string

--- arch/riscv/Makefile.mk
+++ arch/riscv/Makefile.mk
@@ -6,7 +6,9 @@ CC=riscv64-unknown-elf-gcc
 
 # for now, compile common/barrier.* using our own barrier implementations.
 # if your compiler cant handle march, you're using an older version of gcc
-CFLAGS=-Wa,-march=RVIMAFDXhwacha -std=gnu99 -O2 -ffast-math -funroll-loops -I../common 
+#CFLAGS=-Wa,-march=RVIMAFDXhwacha -std=gnu99 -O2 -ffast-math -funroll-loops -I../common 
+CFLAGS=-std=gnu99 -O2 -ffast-math -funroll-loops -I../common 
+
 #CFLAGS=-std=c99 -mtune=native -march=native -mssse3 -O3 -funroll-loops -I../common -DWITH_BARRIER -Wall
 	
 LD_FLAGS= -ffast-math
