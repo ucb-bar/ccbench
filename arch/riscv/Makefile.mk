751387481520eba5cd76a7c598c92b6b1c4819e4
Albert Ou <aou@eecs.berkeley.edu>
2020-01-25 22:58:34 -0800

    Update riscv compiler prefix

--- arch/riscv/Makefile.mk
+++ arch/riscv/Makefile.mk
@@ -2,13 +2,13 @@
 # Architecture-specific Makefile fragment
 ##########################################
 
-CC=riscv-gcc
+CC=riscv64-unknown-elf-gcc
 
 # for now, compile common/barrier.* using our own barrier implementations.
 # if your compiler cant handle march, you're using an older version of gcc
-CFLAGS=-Wa,-march=RVIMAFDXhwacha -std=gnu99 -O2 -ffast-math -funroll-loops -I../common 
+CFLAGS= -std=gnu99 -O2 -ffast-math -funroll-loops -I../common
 #CFLAGS=-std=c99 -mtune=native -march=native -mssse3 -O3 -funroll-loops -I../common -DWITH_BARRIER -Wall
 	
-LD_FLAGS= -ffast-math
+LD_FLAGS= -ffast-math -static
 LD_LIBS=-lm -lc
 
