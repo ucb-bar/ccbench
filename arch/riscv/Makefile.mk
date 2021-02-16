a155dbd5227f89a688a2d82a1a959983121c5b47
Albert Ou <aou@eecs.berkeley.edu>
2021-02-16 12:12:57 -0800

    Compile with medany code model to allow bare-metal linkage

--- arch/riscv/Makefile.mk
+++ arch/riscv/Makefile.mk
@@ -6,7 +6,7 @@ CC=riscv64-unknown-elf-gcc
 
 # for now, compile common/barrier.* using our own barrier implementations.
 # if your compiler cant handle march, you're using an older version of gcc
-CFLAGS= -std=gnu99 -O2 -ffast-math -funroll-loops -I../common
+CFLAGS= -std=gnu99 -O2 -ffast-math -funroll-loops -I../common -mcmodel=medany -g
 #CFLAGS=-std=c99 -mtune=native -march=native -mssse3 -O3 -funroll-loops -I../common -DWITH_BARRIER -Wall
 	
 LD_FLAGS= -ffast-math -static
